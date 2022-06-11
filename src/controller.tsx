c7d2ae775abc51d4f81d1873d75479d9e3d00f94
Kris Baumgartner <kris@trine.la>
2022-06-10 19:42:10 -0700

    More typing

--- src/controller.tsx
+++ src/controller.tsx
@@ -1,20 +1,20 @@
-import { Controller, KeyboardDevice } from '@hmans/controlfreak'
-import create, { StateSelector } from 'zustand'
+import { Controller, KeyboardDevice } from '@hmans/controlfreak';
+import create, { StateSelector } from 'zustand';
 
 interface ControllerState {
-  controller: Controller,
-  keyboard: KeyboardDevice
+  controller: Controller;
+  keyboard: KeyboardDevice;
 }
 
 const useStore = create<ControllerState>((set, get) => {
-  const controller = new Controller()
-  const keyboard = new KeyboardDevice()
-  controller.addDevice(keyboard)
+  const controller = new Controller();
+  const keyboard = new KeyboardDevice();
+  controller.addDevice(keyboard);
 
-  return { controller, keyboard }
-})
+  return { controller, keyboard };
+});
 
-export const useController = (selector?: StateSelector<any, Controller>) => {
-  const controller = useStore(selector ?? ((state) => state.controller))
-  return controller
-}
+export const useController = (selector?: StateSelector<any, any>) => {
+  const controller = useStore(selector ?? ((state) => state.controller));
+  return controller;
+};
--- src/player-controller.tsx
+++ src/player-controller.tsx
@@ -1,12 +1,12 @@
-import { VectorControl, BooleanControl } from '@hmans/controlfreak'
-import { useFrame } from '@react-three/fiber'
-import { useEffect, useRef, useState } from 'react'
-import { useController } from './controller'
-import { Stages } from './app'
-import * as THREE from 'three'
-import { createMachine } from 'xstate'
-import { useMachine } from '@xstate/react'
-import { useStore } from './store'
+import { VectorControl, BooleanControl, Controller, KeyboardDevice } from '@hmans/controlfreak';
+import { useFrame } from '@react-three/fiber';
+import { useEffect, useRef, useState } from 'react';
+import { useController } from './controller';
+import { Stages } from './app';
+import * as THREE from 'three';
+import { createMachine } from 'xstate';
+import { useMachine } from '@xstate/react';
+import { useStore } from './store';
 
 const playerControlsMachine = createMachine({
   id: 'player-controls',
@@ -14,98 +14,108 @@ const playerControlsMachine = createMachine({
   states: {
     idling: {
       on: {
-        RUN: { target: 'running' }
-      }
+        RUN: { target: 'running' },
+      },
     },
     running: {
       on: {
-        IDLE: { target: 'idling' }
-      }
+        IDLE: { target: 'idling' },
+      },
     },
-    jumping: {}
-  }
-})
+    jumping: {},
+  },
+});
 
-const bindPlayerControls = (controller, keyboard) => {
-  controller.addControl('move', VectorControl).addStep(keyboard.compositeVector('KeyW', 'KeyS', 'KeyA', 'KeyD'))
-  controller.addControl('jump', BooleanControl).addStep(keyboard.whenKeyPressed('Space'))
+const bindPlayerControls = (controller: Controller, keyboard: KeyboardDevice) => {
+  controller.addControl('move', VectorControl).addStep(keyboard.compositeVector('KeyW', 'KeyS', 'KeyA', 'KeyD'));
+  controller.addControl('jump', BooleanControl).addStep(keyboard.whenKeyPressed('Space'));
 
   return () => {
-    controller.removeControl('move')
-    controller.removeControl('jump')
-  }
-}
+    controller.removeControl('move');
+    controller.removeControl('jump');
+  };
+};
+
+type Controls = {
+  move: {
+    forward: boolean;
+    backward: boolean;
+    left: boolean;
+    right: boolean;
+  };
+  jump: boolean;
+};
 
 export function usePlayerControls() {
-  const controls = useRef({
+  const controls = useRef<Controls>({
     move: {
       forward: false,
       backward: false,
       left: false,
-      right: false
+      right: false,
     },
-    jump: false
-  })
-  const controller = useController()
+    jump: false,
+  });
+  const controller = useController();
 
   useFrame(() => {
-    const forward = controller.controls.move?.value.y > 0 ?? false
-    const backward = controller.controls.move?.value.y < 0 ?? false
-    const left = controller.controls.move?.value.x < 0 ?? false
-    const right = controller.controls.move?.value.x > 0 ?? false
-    const jump = controller.controls.jump?.value ?? false
-    controls.current = { move: { forward, backward, left, right }, jump }
-  }, Stages.Early)
+    const forward = controller.controls.move?.value.y > 0 ?? false;
+    const backward = controller.controls.move?.value.y < 0 ?? false;
+    const left = controller.controls.move?.value.x < 0 ?? false;
+    const right = controller.controls.move?.value.x > 0 ?? false;
+    const jump = controller.controls.jump?.value ?? false;
+    controls.current = { move: { forward, backward, left, right }, jump };
+  }, Stages.Early);
 
-  return controls
+  return controls;
 }
 
-export function PlayerController({ children }) {
-  const playerRef = useRef(null)
-  const [temp] = useState(() => ({ vec: new THREE.Vector3(), box: new THREE.Box3() }))
-  const directionVec = { forward: [0, 0, -1], backward: [0, 0, 1], left: [-1, 0, 0], right: [1, 0, 0] }
-  const [upVec] = useState(() => new THREE.Vector3(0, 1, 0))
+export function PlayerController({ children }: { children: React.ReactNode }) {
+  const playerRef = useRef<THREE.Group>(null!);
+  const [temp] = useState(() => ({ vec: new THREE.Vector3(), box: new THREE.Box3() }));
+  const directionVec = { forward: [0, 0, -1], backward: [0, 0, 1], left: [-1, 0, 0], right: [1, 0, 0] };
+  const [upVec] = useState(() => new THREE.Vector3(0, 1, 0));
 
-  const [controller, keyboard] = useController((state) => [state.controller, state.keyboard])
-  const [fsm, send] = useMachine(playerControlsMachine)
-  const collider = useStore((state) => state.collider)
+  const [controller, keyboard] = useController((state) => [state.controller, state.keyboard]);
+  const [fsm, send] = useMachine(playerControlsMachine);
+  const collider = useStore((state) => state.collider);
 
-  const playerSpeed = 6
-  const controls = usePlayerControls()
+  const playerSpeed = 6;
+  const controls = usePlayerControls();
 
-  const once = useRef(true)
+  const once = useRef(true);
 
   // Bind move and jump controls
-  useEffect(() => bindPlayerControls(controller, keyboard), [controller, keyboard])
+  useEffect(() => bindPlayerControls(controller, keyboard), [controller, keyboard]);
 
   useFrame((state, delta) => {
-    const { move } = controls.current
-    const isMoving = move.forward | move.backward | move.left | move.right
+    const { move } = controls.current;
+    const isMoving = move.forward || move.backward || move.left || move.right;
 
     if (isMoving) {
-      send('RUN')
+      send('RUN');
 
       // Get the angle between the camera and character
       const angle = Math.atan2(
         state.camera.position.x - playerRef.current.position.x,
-        state.camera.position.z - playerRef.current.position.z
-      )
+        state.camera.position.z - playerRef.current.position.z,
+      );
 
       // Loop over move directions and apply movement to the character
       for (const direction in move) {
-        if (!move[direction]) continue
-        temp.vec.set(...directionVec[direction]).applyAxisAngle(upVec, angle)
-        playerRef.current.position.addScaledVector(temp.vec, playerSpeed * delta)
+        if (!move[direction]) continue;
+        temp.vec.set(...directionVec[direction]).applyAxisAngle(upVec, angle);
+        playerRef.current.position.addScaledVector(temp.vec, playerSpeed * delta);
       }
     } else {
-      send('IDLE')
+      send('IDLE');
     }
-  }, Stages.Update)
+  }, Stages.Update);
 
   useFrame(() => {
-    if (!collider?.geometry?.boundsTree) return
-    if (!once.current) return
-    temp.box.setFromObject(playerRef.current)
+    if (!collider?.geometry?.boundsTree) return;
+    if (!once.current) return;
+    temp.box.setFromObject(playerRef.current);
     // console.log(collider.geometry.boundsTree)
     // collider.geometry.boundsTree.shapecast({
     //   intersectBounds: (box) => box.intersectsBox(temp.box),
@@ -113,8 +123,8 @@ export function PlayerController({ children }) {
     //     console.log('help')
     //   }
     // })
-    once.current = false
-  }, Stages.Update)
+    once.current = false;
+  }, Stages.Update);
 
-  return <group ref={playerRef}>{children}</group>
+  return <group ref={playerRef}>{children}</group>;
 }
