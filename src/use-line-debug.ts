081fc78b190d3271612f008aa94a3541d6c844cd
Kris Baumgartner <kjbaumgartner@gmail.com>
2022-06-13 21:58:07 -0700

    chore(use-line-debug): Cleanup

--- src/use-line-debug.ts
+++ src/use-line-debug.ts
@@ -1,33 +1,33 @@
 import { useFrame, useThree } from '@react-three/fiber';
-import { MutableRefObject, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { Stages } from './app';
 
-export function useLineDebug(line: THREE.Line3 | null = null) {
+export function useLineDebug(line3: THREE.Line3 | null = null) {
   const lineRef = useRef<THREE.Line>(null!);
   const scene = useThree((state) => state.scene);
 
   useEffect(() => {
-    if (!line) return;
+    if (!line3) return;
     const points = [];
-    points.push(line.start);
-    points.push(line.end);
+    points.push(line3.start);
+    points.push(line3.end);
 
     const geometry = new THREE.BufferGeometry().setFromPoints(points);
-    const _line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: 'red', depthTest: false }));
-    scene.add(_line);
-    lineRef.current = _line;
+    const line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: 'red', depthTest: false }));
+    scene.add(line);
+    lineRef.current = line;
 
     return () => {
-      scene.remove(_line);
+      scene.remove(line);
     };
   }, []);
 
   useFrame(() => {
-    if (lineRef.current && line) {
+    if (lineRef.current && line3) {
       const points = [];
-      points.push(line.start);
-      points.push(line.end);
+      points.push(line3.start);
+      points.push(line3.end);
       const geometry = new THREE.BufferGeometry().setFromPoints(points);
       lineRef.current.geometry = geometry;
     }
