269fc71de1592b2417f8ce5839393622acb4708a
Kris Baumgartner <kris@trine.la>
2022-08-15 13:14:07 -0700

    refactor: simplify capsule geometry

--- src/test-assets/player.tsx
+++ src/test-assets/player.tsx
@@ -1,5 +1,5 @@
 import { Cylinder, useHelper } from '@react-three/drei';
-import { useMemo, useRef } from 'react';
+import { useRef } from 'react';
 import * as THREE from 'three';
 
 type PlayerProps = {
@@ -10,13 +10,12 @@ type PlayerProps = {
 export function Player({ radius = 0.5, length = 0.65 }: PlayerProps) {
   const playerRef = useRef<THREE.Mesh>(null!);
 
-  const geometry = useMemo(() => new THREE.CapsuleBufferGeometry(radius, length, 8, 16), [length, radius]);
-
   useHelper(playerRef, THREE.BoxHelper, 'purple');
 
   return (
     <>
-      <mesh ref={playerRef} geometry={geometry}>
+      <mesh ref={playerRef}>
+        <capsuleGeometry args={[radius, length, 8, 16]} />
         <meshStandardMaterial />
       </mesh>
       {/* <Cylinder ref={playerRef} args={[0.5, 0.5, 2, 16]} /> */}
