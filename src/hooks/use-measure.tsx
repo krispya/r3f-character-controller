00cc4f3ac34f9987e121a84e723d72cf236b38f2
Kris Baumgartner <kris@trine.la>
2022-08-15 13:12:26 -0700

    refactor: use-measure now takes a callback

--- src/hooks/use-measure.tsx
+++ src/hooks/use-measure.tsx
@@ -1,19 +1,21 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as THREE from 'three';
 
-export function useMeasure(): [React.Ref<THREE.Mesh>, THREE.Vector3, THREE.Box3] {
-  const ref = useRef<THREE.Mesh>(null!);
-  const [size, setSize] = useState(() => new THREE.Vector3(0, 0, 0));
-  const [box, setBox] = useState(() => new THREE.Box3(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0)));
+export type MeasureHandler = (size: THREE.Vector3, box: THREE.Box3) => void;
+type MeasureOptions = { precise?: boolean };
+
+export function useMeasure(
+  ref: React.MutableRefObject<THREE.Object3D>,
+  callback: MeasureHandler,
+  options?: MeasureOptions,
+) {
+  const [size] = useState(() => new THREE.Vector3(0, 0, 0));
+  const [box] = useState(() => new THREE.Box3(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0)));
 
   useEffect(() => {
     if (!ref.current) return;
-    const _box = new THREE.Box3().setFromObject(ref.current);
-    const _size = new THREE.Vector3();
-    _box.getSize(_size);
-    setSize(_size);
-    setBox(_box);
-  }, [ref]);
-
-  return [ref, size, box];
+    box.setFromObject(ref.current, options?.precise);
+    box.getSize(size);
+    callback(size, box);
+  }, [box, callback, options?.precise, ref, size]);
 }
